1f557c47045297ef49bb75e7cee5fe38f23a394f
daveripper0020 <daveripper0020@gmail.com>
2022-12-05 01:27:27 +0900

    병렬 스크린샷 테스트 중

--- src/components/ImageBlock.svelte
+++ src/components/ImageBlock.svelte
@@ -1,30 +1,28 @@
 <script lang="ts">
     export let align: string = "left";
     export let type: string = "horizontal";
+    export let allowImagesOverflow: boolean = false;
 </script>
 
-<div class={type}>
-{#if align == "left" || align == "top"}
-    <slot name="image" />
-{/if}
+<div class={type} class:big-images-container={allowImagesOverflow}>
+    {#if align == "left" || align == "top"}
+    <div class="images" class:big-images={allowImagesOverflow}>
+        <slot name="image" />
+    </div>
+    {/if}
     <div class="text">
         <slot />
     </div>
-{#if align == "right" || align == "bottom"}
-    <slot name="image" />
-{/if}
+    {#if align == "right" || align == "bottom"}
+    <div class="images" class:big-images={allowImagesOverflow}>
+        <slot name="image" />
+    </div>
+    {/if}
 </div>
 
 <style>
     .horizontal {
         display: flex;
-    }
-
-    @media (width <= 600px) {
-        .horizontal {
-            flex-direction: column;
-            align-items: center;
-        }
     }
 
     .vertical {
@@ -33,7 +31,21 @@
         flex-direction: column;
     }
 
-    @media (width <= 600px) {
+    .big-images-container {
+        margin: 0 calc(((70rem - 100%) / 2 * -1) - 1.25rem);
+    }
+
+    .big-images {
+        width: fit-content;
+        margin: 0 auto;
+    }
+
+    @media (max-width: 60rem) {
+        .horizontal {
+            flex-direction: column;
+            align-items: center;
+        }
+
         .vertical {
             width: 20em;
         }
