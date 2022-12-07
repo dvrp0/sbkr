6c7d631a9b5fa17845daece3db51cf0f235937fc
daveripper0020 <daveripper0020@gmail.com>
2022-12-07 12:22:45 +0900

    인덱스 페이지 임시 추가

---
+++ src/lib/server/article.ts
@@ -0,0 +1,40 @@
+import fss from "fs";
+import fs from "fs/promises";
+import yaml from "js-yaml";
+
+export type ArticleMeta = {
+    id: string;
+    title: string;
+    publishedAt: string;
+    headerImage: string;
+    summary: string;
+    draft: boolean;
+};
+
+export function extractMeta(id: string, markdown: string): ArticleMeta
+{
+    const seperator = '---\n';
+    const raw = markdown.substring(seperator.length, markdown.indexOf(seperator, seperator.length)).trim();
+    const frontmatter = yaml.load(raw) as Record<string, any>;
+
+    return {
+        ...frontmatter,
+        id,
+        draft: !!frontmatter.draft
+    } as ArticleMeta;
+}
+
+export async function getArticleMeta(dir: string, id: string): Promise<ArticleMeta>
+{
+    const f = await fs.readFile(`${dir}/${id}/+page.md`);
+
+    return extractMeta(id, f.toString());
+}
+
+export async function getArticleMetas(dir: string): Promise<ArticleMeta[]>
+{
+    const promises = (await fs.readdir(dir)).filter(x => fss.existsSync(`${dir}/${x}/+page.md`))
+                                            .map(x => getArticleMeta(dir, x));
+
+    return (await Promise.all(promises)).sort((x, y) => x.publishedAt < y.publishedAt ? 1 : -1);
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import type { PageData } from "./$types"
+
+    export let data: PageData
+
+    let posts = data.posts;
+</script>
+
+<svelte:head>
+    <title>스톰바운드 한국어 패치 노트</title>
+</svelte:head>
+
+<h1>스톰바운드 한국어 패치 노트</h1>
+
+<ul class="posts">
+    {#each posts as post}
+    <article>
+        <img class="header-image" alt="메인 이미지" src={post.headerImage} />
+        <h2 class="title"><a href={`/posts/${post.id}`}>{post.title}</a></h2>
+        <p class="published-at">{post.publishedAt}</p>
+        <p class="summary">{post.summary}</p>
+    </article>
+    {/each}
+</ul>
