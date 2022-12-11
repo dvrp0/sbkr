37772edf0a5e2ac9175dca05cf2a2c95a2da6ce6
daveripper0020 <daveripper0020@gmail.com>
2022-12-11 21:57:12 +0900

    주석 컴포넌트 추가

---
+++ src/components/Comment.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+</script>
+
+<div class="comment">
+    <div class="inner">
+        <slot />
+    </div>
+</div>
+
+<style>
+    .comment {
+        background-color: var(--c-background-light);
+        color: var(--c-foreground-comment);
+    }
+
+    .inner {
+        padding: var(--padding);
+    }
+</style>
