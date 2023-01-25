b01a8f24f78d31f26afc4162db33ed042ba28828
daveripper0020 <daveripper0020@gmail.com>
2023-01-26 03:11:01 +0900

    SSG로 복귀, 사용량 페이지 load 최적화

--- src/routes/card-usages/[league]/+page.server.ts
+++ src/routes/card-usages/[league]/+page.server.ts
@@ -1,25 +1,31 @@
 import { getCardUsagesMetas, type ArticleMeta } from "$lib/server/article";
 import type { PageServerLoad } from "./$types";
 
-export const prerender = false;
+// export const prerender = false;
 
 export const load: PageServerLoad = async ({ fetch, params }) => {
     const meta = getCardUsagesMetas().find(x => x.id == params.league) as ArticleMeta;
 
-    let response = await fetch("https://sbkrserver.deta.dev/usages/");
-    const usage = (await response.json())["result"][params.league] as JSON;
-    
-    response = await fetch("https://sbkrserver.deta.dev/usage-changes/");
-    const change = (await response.json())["result"][params.league] as JSON;
-    
-    response = await fetch("https://sbkrserver.deta.dev/translations")
-    const translations = (await response.json())["result"] as JSON;
+    const usage = async () => {
+        const response = await fetch(`https://sbkrserver.deta.dev/usages?league=${params.league}`);
+        return (await response.json())["result"] as JSON;
+    }
+
+    const change = async () => {
+        const response = await fetch(`https://sbkrserver.deta.dev/usage-changes?league=${params.league}`);
+        return (await response.json())["result"] as JSON;
+    }
+
+    const translations = async () => {
+        const response = await fetch("https://sbkrserver.deta.dev/translations");
+        return (await response.json())["result"] as JSON;
+    }
 
     return {
         ...meta,
         league: params.league,
-        usage,
-        change,
-        translations
+        usage: usage(),
+        change: change(),
+        translations: translations()
     };
 }
