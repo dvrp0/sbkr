3ba16ebf81146163dfc3e14473548cb95d55578f
daveripper0020 <daveripper0020@gmail.com>
2023-04-29 00:01:03 +0900

    5월 패치 노트 수정

--- src/lib/server/article.ts
+++ src/lib/server/article.ts
@@ -1,5 +1,3 @@
-import yaml from "js-yaml";
-
 export type ArticleMeta = {
     id: string;
     title: string;
@@ -17,13 +15,11 @@ export function getPatchNotesMetas(): ArticleMeta[]
     const posts = Object.entries(globs);
     
     return posts
-        .map(x => {
-            return {
+        .map(x => ({
                 ...x[1].metadata,
                 id: x[0].split("releases/")[1].substring(0, 7),
                 draft: !!x[1].metadata.draft
-            } as ArticleMeta;
-        })
+            } as ArticleMeta))
         .filter(x => !x.draft)
         .sort((x, y) => x.publishedAt < y.publishedAt ? 1 : -1);
 }
@@ -40,18 +36,12 @@ export function getCardUsagesMetas(): ArticleMeta[]
         ["iron", "아이언"],
         ["starters", "스타터"]
     ];
-    let result: ArticleMeta[] = [];
 
-    for (const league of leagues)
-    {
-        result.push({
-            title: `${league[1]} 리그 카드 사용량`,
-            headerImage: `/images/headers/header_${league[0]}.png`,
-            summary: `최근 30일 동안 ${league[1]} 리그에서 가장 많이 사용된 카드 목록입니다.`,
-            source: `https://stormbound-kitty.com/card-usage/${league[0]}`,
-            id: league[0]
-        } as ArticleMeta);
-    }
-
-    return result;
+    return leagues.map(league => ({
+        title: `${league[1]} 리그 카드 사용량`,
+        headerImage: `/images/headers/header_${league[0]}.png`,
+        summary: `최근 30일 동안 ${league[1]} 리그에서 가장 많이 사용된 카드 목록입니다.`,
+        source: `https://stormbound-kitty.com/card-usage/${league[0]}`,
+        id: league[0]
+    } as ArticleMeta));
 }
