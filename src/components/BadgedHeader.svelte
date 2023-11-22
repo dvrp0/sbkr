d608994bfb1489c01d37161ee0ac9d0a59ee82cc
daveripper0020 <daveripper0020@gmail.com>
2023-11-22 23:37:38 +0900

    BadgedHeader 컴포넌트 추가

---
+++ src/components/BadgedHeader.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import Badge from "$components/Badge.svelte";
+
+    export let heading: string;
+    export let badge: string;
+</script>
+
+<div class="container">
+    <h2>{heading}</h2>
+    <div class="badge-wrapper">
+        <Badge>{badge}</Badge>
+    </div>
+</div>
+
+<style>
+    .container {
+        display: flex;
+        align-items: flex-start;
+    }
+
+    .badge-wrapper {
+        margin-top: 4.25em;
+        margin-left: 1em;
+    }
+</style>
